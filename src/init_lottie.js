f1e9a1168c96d1769d1666132e0cae614f287063
FfEzT <vl.al.kim@mail.ru>
2021-12-24 23:45:41 +0500

    0

---
+++ src/init_lottie.js
@@ -0,0 +1,26 @@
+// for web
+let animation = bodymovin.loadAnimation(
+    {
+        container: document.getElementById('***'),
+        rederer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '***.json'
+    }
+)
+
+
+
+
+// for node.js
+let lottie = require("lottie-web")
+
+let animation = lottie.loadAnimation(
+    {
+        container: document.getElementById('***'),
+        rederer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '***.json'
+    }
+)
